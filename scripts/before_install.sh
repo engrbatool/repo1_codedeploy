2685f39d55f84787a7532713f1082789249cb387
Ubuntu <ubuntu@ip-172-31-29-132.ec2.internal>
2019-09-04 23:13:10 +0000

    add venv install packages

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,9 @@ echo "i am before_install.sh"
 # Install libaries
 whoami
 pwd
+apt-get upgrade 
+apt-get install -y python3-pip build-essential libssl-dev libffi-dev python3-dev python3-venv
+echo "packages installed successfully"
 mkdir /tutorialmyprojectdir
 cd /tutorialmyprojectdir
 virtualenv -p python3 venv
