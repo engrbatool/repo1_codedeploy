9de3cfa1e96941f6f5baa0401e416c0b347c1c0b
Ubuntu <ubuntu@ip-172-31-29-132.ec2.internal>
2019-09-04 23:26:48 +0000

    creating tutorialdir if not exist

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,7 +6,7 @@ pwd
 apt-get upgrade 
 apt-get install -y python3-pip build-essential libssl-dev libffi-dev python3-dev python3-venv
 echo "packages installed successfully"
-mkdir /tutorialmyprojectdir
+[ -d /tutorialmyprojectdir ] || mkdir /tutorialmyprojectdir
 cd /tutorialmyprojectdir
 virtualenv -p python3 venv
 source venv/bin/activate
